e62606132d62197ba69b4a8e8b0dd8644da6d22a
k-i-a <nomadnotmad>
2021-05-03 08:27:43 +0300

    45

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -1,43 +1,38 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BruteCollinearPoints {
-    private int numberOfSegments = 0;
-    private LineSegment[] lineSegment;
+    private ArrayList<LineSegment> lineSegments = new ArrayList<>();
     // finds all line segments containing 4 points
     public BruteCollinearPoints(Point[] points) {
-        lineSegment = new LineSegment[points.length/4];
-        for (int i0 = 0; i0 < points.length - 3; ++i0) {
-            for (int i1 = i0 + 1; i1 < points.length - 2; ++i1) {
-                for (int i2 = i1 + 1; i2 < points.length - 1; ++i2) {
-                    if (points[i0].slopeTo(points[i1]) == points[i0].slopeTo(points[i2])) {
+        if (points == null) throw new IllegalArgumentException();
+        for (int i = 0; i + 1 < points.length; ++i) {
+            if (points[i] == null) throw new IllegalArgumentException();
+            if (points[i].compareTo(points[i + 1]) == 0) throw new IllegalArgumentException();
+        }
+        Point[] pointsArray = points.clone();
+        Arrays.sort(pointsArray);
+        for (int i0 = 0; i0 < pointsArray.length - 3; ++i0) {
+            for (int i1 = i0 + 1; i1 < pointsArray.length - 2; ++i1) {
+                for (int i2 = i1 + 1; i2 < pointsArray.length - 1; ++i2) {
+                    if (pointsArray[i0].slopeTo(pointsArray[i1]) == pointsArray[i0].slopeTo(pointsArray[i2])) {
                         for (int i3 = i2 + 1; i3 < points.length; ++i3) {
-                            if (points[i0].slopeTo(points[i1])  == points[i0].slopeTo(points[i3])) {
-                                Point[] pointsSegment = new Point[4];
-                                pointsSegment[0] = points[i0];
-                                pointsSegment[1] = points[i1];
-                                pointsSegment[2] = points[i2];
-                                pointsSegment[3] = points[i3];
-                                Arrays.sort(pointsSegment);
-                                lineSegment[numberOfSegments] = new LineSegment(pointsSegment[0], pointsSegment[3]);
-                                numberOfSegments++;
+                            if (pointsArray[i0].slopeTo(pointsArray[i1])  == pointsArray[i0].slopeTo(pointsArray[i3])) {
+                                lineSegments.add(new LineSegment(pointsArray[i0], pointsArray[i3]));
                             }
                         }
                     }
                 }
             }
         }
-        LineSegment[] buffLineSegment = lineSegment;
-        lineSegment = new LineSegment[numberOfSegments];
-        for (int i = 0; i < lineSegment.length; ++i) {
-            lineSegment[i] = buffLineSegment[i];
-        }
+
     }
     // the number of line segments
     public int numberOfSegments() {
-        return numberOfSegments;
+        return lineSegments.size();
     }
     // the line segments
     public LineSegment[] segments() {
-        return lineSegment;
+        return lineSegments.toArray(new LineSegment[lineSegments.size()]);
     }
 }
--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -1,7 +1,13 @@
+import java.util.Arrays;
+
 public class FastCollinearPoints {
     // finds all line segments containing 4 or more points
     public FastCollinearPoints(Point[] points) {
-
+        if (points == null) throw new IllegalArgumentException();
+        for (int i = 0; i + 1 < points.length; ++i) {
+            if (points[i] == null) throw new IllegalArgumentException();
+            if (points[i].compareTo(points[i + 1]) == 0) throw new IllegalArgumentException();
+        }
     }
     // the number of line segments
     public int numberOfSegments() {
